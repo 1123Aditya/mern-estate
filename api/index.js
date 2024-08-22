209e1986bd01221cfe48fb48797a17f071443dca
Aditya Jadhav <aj2201055@gmail.com>
2024-08-22 23:19:02 +0530

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+ .connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDb..");
+}).catch((Err)=>{
+    console.log(Err);
+})
+
 const app = express();
 
+
 app.listen(3000,()=>{
-    console.log("server is listining at port 3000!");
+    console.log("server is listining at port 3000");
 });
